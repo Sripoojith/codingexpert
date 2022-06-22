8a91a0d8ad43bd80474351383769b510f63e98b8
Sripoojith <103428046+Sripoojith@users.noreply.github.com>
2022-06-22 20:03:01 +0530

    Create config.php

---
+++ Ntask2/config.php
@@ -0,0 +1,15 @@
+<?php
+
+  $host = "localhost";
+
+  $dbUsername = "root";
+
+  $dbPassword = "";
+
+  $dbName = "user1";
+
+  
+
+  $mysqli = mysqli_connect($host, $dbUsername, $dbPassword, $dbName);
+
+?>
